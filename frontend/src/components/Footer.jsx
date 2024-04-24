1121be07880e860a6c073caa04f986504011cbad
Shreya Jash <shreyajash98@gmail.com>
2024-04-25 01:14:59 +0530

    feat: footer updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,6 @@
 
 export default function Footer() {
   return (
-    <div className="px-44 h-[400px] bg-[#043873]" id="footer">Footer</div>
+    <div className="px-44 h-[50px] bg-[#043873] text-center text-white pt-3" id="footer">© 2024 Copyright: Emotune</div>
   )
 }
