3cf82836256189a8e46c1f7e27dc1f22fdf82469
Shreya Jash <shreyajash98@gmail.com>
2024-04-27 01:52:59 +0530

    feat

--- backend/database.sql
+++ backend/database.sql
@@ -3,10 +3,9 @@ CREATE DATABASE loginauth;
 
 --setExtention
 CREATE TABLE users(
-    user_id uuid PRIMARY KEY DEFAULT
-    uuid_generate_v4(),
-    user_name VARCHAR(12) NOT NULL,
-    user_password VARCHAR(12) NOT NULL
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_name TEXT NOT NULL,
+    user_password TEXT NOT NULL
 );
 
 --insert fake users
--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // gonna help us connect server with database
 const Pool = require("pg").Pool;
 
-const pool = new Pool(process.env.DB_URL);
+const pool = new Pool({connectionString: process.env.DB_URL});
 console.log(pool)
 
 module.exports = pool;
