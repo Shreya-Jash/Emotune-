40888086443eb32f58c5d2f5864972ab7a47f463
Shreya Jash <shreyajash98@gmail.com>
2024-04-27 02:27:23 +0530

    feat:bug fix

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,13 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 const url=process.env.DB_URL
 console.log(url)
-const pool = new Pool({connectionString: url});
+const ssl = {
+    rejectUnauthorized: false // You may need to adjust this based on your SSL/TLS configuration
+  };
+const pool = new Pool({
+    connectionString: url,
+    ssl: ssl
+});
 pool.query('SELECT 1', (err, res) => {
     if (err) {
       console.error('Error connecting to database:', err);
